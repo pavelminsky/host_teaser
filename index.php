9f78ae834c1f2b2ab3dd9bec22f915bc1813c1ab
pavelminsky <pavelminsky@gmail.com>
2013-03-15 15:45:00 +0400

    initial upload

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><title>Гамбит — В кино с 7 марта</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Гамбит. Официальный сайт фильма. В кино с 7 марта.">
+<meta name="keywords" content="Гамбит, Gambit, криминальная комедия, фильм, Майкл Хоффман, Кэмерон Диаз, Колин Ферт, Алан Рикман, Стэнли Туччи, братья Коэны, Джоэл Коэн, Итан Коэн">
+
+<meta property="og:title" content="Гамбит - официальный сайт фильма">
+<meta property="og:description" content="Искусствовед-авантюрист, звезда родео, злобный миллиардер и поддельное полотно Моне в римейке одноименной криминальной комедии!">
+<meta property="og:type" content="movie">
+<meta property="og:url" content="http://www.gambitmovie.ru">
+<meta property="og:image" content="http://www.gambitmovie.ru/images/share.jpg">
+<meta property="og:site_name" content="Гамбит - официальный сайт фильма">
+
+<meta name="viewport" content="width=990">
+<meta name="format-detection" content="telephone=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta name="apple-mobile-web-app-title" content="Гамбит">
+<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="apple-touch-icon-precomposed" href="/apple-touch-icon.png">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="/favicon.png">
+
+<meta itemprop="name" content="Гамбит - официальный сайт фильма">
+<meta itemprop="description" content="Искусствовед-авантюрист, звезда родео, злобный миллиардер и поддельное полотно Моне в римейке одноименной криминальной комедии!">
+<meta itemprop="image" content="http://www.gambitmovie.ru/images/share.jpg">
+
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="/css/fancybox.css?v=2.1.1" media="screen">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,300&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="/js/init.js"></script>
+<script>
+jQuery(function($) {
+$('.dim').fadeOut(500);
+});
+</script>
+
+<link href="/css/video-js.css" rel="stylesheet">
+<script type="text/javascript" src="/js/video.js"></script>
+<style type="text/css">
+.vjs-default-skin .vjs-play-progress {background: #969696;}
+.vjs-default-skin .vjs-load-progress {background: #5a5a5a;}
+.vjs-default-skin .vjs-seek-handle {display: none;}
+.vjs-default-skin .vjs-current-time-display, .vjs-default-skin .vjs-remaining-time-display {font: normal 10px Arial; color: #6f7374; text-shadow: none;}
+</style>
+
+<meta name='wmail-verification' content='c4e98587352b7f4d' />
+<meta name="msvalidate.01" content="BA8384586740DC05834E57B6B5DAB8B5" />
+<meta name='yandex-verification' content='45f5aad489aba223' />
+<meta name="google-site-verification" content="UXf09ugc39bEsld90SGyQ2D4LurnEAJDiEzd4GmBHhY" />
+
+</head><body>
+
+<noscript><style>.dim {display: none;}</style></noscript>
+<div class="dim">&nbsp;</div>
+
+<div class="header noselect">
+	<div class="cul">&nbsp;</div>
+	<div class="cover">&nbsp;</div>
+	<div class="cur">&nbsp;</div>
+</div>
+
+<div id="gambit_russiantrailer_550" style="width: 800px; height: 335px; margin: 0 auto;"><video class="video-js vjs-default-skin" controls preload="auto" width="800" height="335" poster="/video/gambit_russiantrailer_550.jpg" data-setup="{}"><source src="http://www.gambitmovie.ru/video/gambit_russiantrailer_550.mp4" type='video/mp4'><source src="http://www.gambitmovie.ru/video/gambit_russiantrailer_550.ogv" type='video/ogg'></video></div>
+
+<div class="about">
+	<div class="story">
+		<p style="text-indent: -0.6em;"><b>&laquo;Гамбит&raquo;</b> — римейк одноименной криминальной комедии 1966 года.</p>
+		<p>Главный герой фильма — лондонский искусствовед Гарри Дин (<b>Колин Фёрт</b>). Авантюрист задумал обмануть богатого коллекционера Лайонела Шабандара (<b>Алан Рикман</b>), продав ему фальшивую картину Моне.</p>
+		<p>Для этого Гарри вступает в сговор с эксцентричной и разбитной девицей из Техаса по имени Пи-Джей Пузновски (<b>Кэмерон Диаз</b>). Пи-Джей не имеет никакого отношения к живописи — она выступает на родео. Тем не менее, ей придётся выдавать себя за женщину, чей дед спас полотно Моне из нацистской Германии в конце Второй мировой.</p>
+	</div>
+	<div class="cast">
+		<p><b>Режиссер:</b> Майкл Хоффман</p>
+		<p><b>Сценаристы:</b> Джоэл Коэн и Итан Коэн</b>
+		<p><b>В ролях:</b> Кэмерон Диаз, Алан Рикман, Колин Фёрт, Стэнли Туччи, Клорис Личмен, Анна Скеллерн, Том Кортни, Того Игава, Эрика ЛаРоуз, Танрох Ишида</p>
+	</div>
+</div>
+
+<div id="counters"><div class="counters_list"><?php include ("counters_include.php"); ?></div></div>
+
+<div class="footer noselect">
+	<div class="cdl">&nbsp;</div>
+
+	<div class="gallery"><p><a class="stills" data-fancybox-group="gallery_stills" href="/media/01.jpg"><img src="/media/01s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/02.jpg"><img src="/media/02s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/03.jpg"><img src="/media/03s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/04.jpg"><img src="/media/04s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/05.jpg"><img src="/media/05s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/06.jpg"><img src="/media/06s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/07.jpg"><img src="/media/07s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/08.jpg"><img src="/media/08s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/09.jpg"><img src="/media/09s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/10.jpg"><img src="/media/10s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/11.jpg"><img src="/media/11s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/12.jpg"><img src="/media/12s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/13.jpg"><img src="/media/13s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/14.jpg"><img src="/media/14s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/15.jpg"><img src="/media/15s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/16.jpg"><img src="/media/16s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/17.jpg"><img src="/media/17s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/18.jpg"><img src="/media/18s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/19.jpg"><img src="/media/19s.jpg" alt=""></a></p></div>
+
+	<div class="copy">&copy; 2013 &laquo;<a href="http://www.westvideo.ru">Вест Видео</a>&raquo;. Создание сайта: <a href="http://www.kg-design.ru/">КГ-Дизайн</a></div>
+
+	<div class="cdr">&nbsp;</div>
+</div>
+
+<!-- Fancybox -->
+<script type="text/javascript" src="/js/jquery.fancybox.js?v=2.1.1"></script>
+<script type="text/javascript" src="/js/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+<script type="text/javascript" src="/js/jquery.fancybox-media.js?v=1.0.4"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+$('.stills').fancybox({
+//prevEffect : 'none',
+//nextEffect : 'none',
+//closeBtn  : false,
+//arrows    : false,
+nextClick : false,
+'margin' : 50,
+helpers : {thumbs : {width  : 56, height : 40}}
+});
+});
+</script>
+
+</body></html>
