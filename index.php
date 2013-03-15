baaa3b255abd12522d1426e6309060c51748f311
pavelminsky <pavelminsky@gmail.com>
2013-03-15 17:42:12 +0400

    done 1.0.1

--- index.php
+++ index.php
@@ -1,37 +1,37 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head><title>Гамбит — В кино с 7 марта</title>
+<head><title>&laquo;Гостья&raquo; &bull; От автора саги &laquo;Сумерки&raquo; &bull; В кино с 28 марта</title>
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="description" content="Гамбит. Официальный сайт фильма. В кино с 7 марта.">
-<meta name="keywords" content="Гамбит, Gambit, криминальная комедия, фильм, Майкл Хоффман, Кэмерон Диаз, Колин Ферт, Алан Рикман, Стэнли Туччи, братья Коэны, Джоэл Коэн, Итан Коэн">
+<!--meta name="description" content="Название. Официальный сайт фильма. Дата.">
+<meta name="keywords" content="Название, Title, жанры, фильм, режиссер, актеры"-->
 
-<meta property="og:title" content="Гамбит - официальный сайт фильма">
-<meta property="og:description" content="Искусствовед-авантюрист, звезда родео, злобный миллиардер и поддельное полотно Моне в римейке одноименной криминальной комедии!">
+<!--meta property="og:title" content="Название — официальный сайт фильма">
+<meta property="og:description" content="Краткое описание">
 <meta property="og:type" content="movie">
-<meta property="og:url" content="http://www.gambitmovie.ru">
-<meta property="og:image" content="http://www.gambitmovie.ru/images/share.jpg">
-<meta property="og:site_name" content="Гамбит - официальный сайт фильма">
+<meta property="og:url" content="http://www.url.ru">
+<meta property="og:image" content="http://www.url.ru/images/share.jpg">
+<meta property="og:site_name" content="Название — официальный сайт фильма"-->
 
-<meta name="viewport" content="width=990">
+<meta name="viewport" content="width=960">
 <meta name="format-detection" content="telephone=no">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="black">
-<meta name="apple-mobile-web-app-title" content="Гамбит">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<meta name="apple-mobile-web-app-title" content="Гостья">
+<!--link rel="apple-touch-icon" href="/apple-touch-icon.png">
 <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon.png">
-<link rel="icon" href="/favicon.ico" type="image/x-icon">
-<link rel="shortcut icon" href="/favicon.png">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="favicon.png"-->
 
-<meta itemprop="name" content="Гамбит - официальный сайт фильма">
-<meta itemprop="description" content="Искусствовед-авантюрист, звезда родео, злобный миллиардер и поддельное полотно Моне в римейке одноименной криминальной комедии!">
-<meta itemprop="image" content="http://www.gambitmovie.ru/images/share.jpg">
+<!--meta itemprop="name" content="Название — официальный сайт фильма">
+<meta itemprop="description" content="Краткое описание">
+<meta itemprop="image" content="http://www.url.ru/images/share.jpg"-->
 
 <link rel="stylesheet" href="css/main.css">
 <link rel="stylesheet" type="text/css" href="/css/fancybox.css?v=2.1.1" media="screen">
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<link href='http://fonts.googleapis.com/css?family=Roboto:400,300&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300,400&subset=latin,cyrillic-ext,cyrillic,latin-ext' rel='stylesheet' type='text/css'>
 <script type="text/javascript" src="/js/init.js"></script>
 <script>
 jQuery(function($) {
@@ -48,48 +48,74 @@ $('.dim').fadeOut(500);
 .vjs-default-skin .vjs-current-time-display, .vjs-default-skin .vjs-remaining-time-display {font: normal 10px Arial; color: #6f7374; text-shadow: none;}
 </style>
 
-<meta name='wmail-verification' content='c4e98587352b7f4d' />
-<meta name="msvalidate.01" content="BA8384586740DC05834E57B6B5DAB8B5" />
-<meta name='yandex-verification' content='45f5aad489aba223' />
-<meta name="google-site-verification" content="UXf09ugc39bEsld90SGyQ2D4LurnEAJDiEzd4GmBHhY" />
+
 
 </head><body>
 
 <noscript><style>.dim {display: none;}</style></noscript>
 <div class="dim">&nbsp;</div>
 
-<div class="header noselect">
-	<div class="cul">&nbsp;</div>
-	<div class="cover">&nbsp;</div>
-	<div class="cur">&nbsp;</div>
+
+
+<style type="text/css">
+a:link, a:visited {color: #cf7600;}
+a:hover {color: #ea8600;}
+
+
+.header {position: relative; width: 80%; min-width: 960px; margin: 0 auto; text-align: center;}
+.footer {position: relative; width: 100%; min-height: 130px; margin: 0 auto; text-align: center;}
+
+.trailer_wrap {position: relative; bottom: 120px; width: 960px; height: 360px; margin: 0 auto -110px auto; text-align: center;}
+
+.about {width: 780px; margin: 30px auto 40px auto; text-align: left; background: url('/images/age.png') left top no-repeat;}
+.story, .cast {margin-left: 160px;}
+.story {padding-bottom: 15px; font-size: 1em; line-height: 1.3em;}
+.story p {padding-bottom: 16px;}
+.cast {font-size: 0.85em; line-height: 1.3em; color: #eddca2;}
+.cast p {padding-bottom: 10px;}
+
+#counters {position: fixed; bottom: -120px; right: 160px; z-index: 3; width: 140px; height: 140px; text-align: right; background: url('/images/counters.png') right top no-repeat;}
+.counters_list {padding-top: 30px; opacity: 0.40; -moz-opacity: 0.40; -khtml-opacity: 0.40; filter: alpha(opacity=40);}
+#counters:hover {bottom: 0;}
+
+.gallery {margin: 0 auto; width: 880px;}
+a.stills:hover {opacity: 0.80; -moz-opacity: 0.80; -khtml-opacity: 0.80; filter: alpha(opacity=80);}
+
+.copy {width: 880px; margin: 0 auto; padding: 80px 10px 40px 10px; font-size: 0.8em; color: #718893; text-align: center;}
+</style>
+
+
+
+<div class="header noSelect">
+	<img src="/images/bg_main.jpg" width="100%" alt="">
+	<div class="trailer_wrap">
+		<div id="host_russiantrailer_550" style="width: 800px; height: 335px; margin: 0 auto; border: 5px solid #c8ecf7;"><video class="video-js vjs-default-skin" controls preload="auto" width="800" height="335" poster="/video/host_russiantrailer_550.jpg" data-setup="{}"><source src="http://www.hostmovie.ru/video/host_russiantrailer_550.mp4" type='video/mp4'><source src="http://www.hostmovie.ru/video/host_russiantrailer_550.ogv" type='video/ogg'></video></div>
+	</div>
 </div>
 
-<div id="gambit_russiantrailer_550" style="width: 800px; height: 335px; margin: 0 auto;"><video class="video-js vjs-default-skin" controls preload="auto" width="800" height="335" poster="/video/gambit_russiantrailer_550.jpg" data-setup="{}"><source src="http://www.gambitmovie.ru/video/gambit_russiantrailer_550.mp4" type='video/mp4'><source src="http://www.gambitmovie.ru/video/gambit_russiantrailer_550.ogv" type='video/ogg'></video></div>
 
 <div class="about">
 	<div class="story">
-		<p style="text-indent: -0.6em;"><b>&laquo;Гамбит&raquo;</b> — римейк одноименной криминальной комедии 1966 года.</p>
-		<p>Главный герой фильма — лондонский искусствовед Гарри Дин (<b>Колин Фёрт</b>). Авантюрист задумал обмануть богатого коллекционера Лайонела Шабандара (<b>Алан Рикман</b>), продав ему фальшивую картину Моне.</p>
-		<p>Для этого Гарри вступает в сговор с эксцентричной и разбитной девицей из Техаса по имени Пи-Джей Пузновски (<b>Кэмерон Диаз</b>). Пи-Джей не имеет никакого отношения к живописи — она выступает на родео. Тем не менее, ей придётся выдавать себя за женщину, чей дед спас полотно Моне из нацистской Германии в конце Второй мировой.</p>
+		<p>Земля — в опасности! Наше место скоро займут Души — лишенные плотской оболочки пришельцы, вытесняющие из человеческих тел разум и замещающие его разумом собственным. Большая часть человечества уже погибла. Немногие выжившие скрываются в жалкой попытке отсрочить неизбежное…</p>
+		<p>Теперь Душа пытается захватить тело юной Мелани. Однако происходит неожиданное: Мелани и ее Душа (Ванда) вынуждены сосуществовать в одном теле.</p>
+		<p>Гостье надлежало выследить и выдать землян-повстанцев, с которыми связана Мелани, но она помогает своей носительнице.</p>
 	</div>
 	<div class="cast">
-		<p><b>Режиссер:</b> Майкл Хоффман</p>
-		<p><b>Сценаристы:</b> Джоэл Коэн и Итан Коэн</b>
-		<p><b>В ролях:</b> Кэмерон Диаз, Алан Рикман, Колин Фёрт, Стэнли Туччи, Клорис Личмен, Анна Скеллерн, Том Кортни, Того Игава, Эрика ЛаРоуз, Танрох Ишида</p>
+		<p><b>Режиссер:</b> Эндрю Никкол</p>
+		<p><b>Сценаристы:</b> Эндрю Никкол и Стефани Майер</p>
+		<p><b>В ролях:</b> Сирша Ронан, Макс Айронс, Джейк Эйбел, Дайан Крюгер, Уильям Хёрт, Фрэнсис Фишер, Бойд Холбрук, Чандлер Кентербери, Райдэн Грир и Андреа Франкл</p>
 	</div>
 </div>
 
 <div id="counters"><div class="counters_list"><?php include ("counters_include.php"); ?></div></div>
 
 <div class="footer noselect">
-	<div class="cdl">&nbsp;</div>
-
-	<div class="gallery"><p><a class="stills" data-fancybox-group="gallery_stills" href="/media/01.jpg"><img src="/media/01s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/02.jpg"><img src="/media/02s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/03.jpg"><img src="/media/03s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/04.jpg"><img src="/media/04s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/05.jpg"><img src="/media/05s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/06.jpg"><img src="/media/06s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/07.jpg"><img src="/media/07s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/08.jpg"><img src="/media/08s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/09.jpg"><img src="/media/09s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/10.jpg"><img src="/media/10s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/11.jpg"><img src="/media/11s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/12.jpg"><img src="/media/12s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/13.jpg"><img src="/media/13s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/14.jpg"><img src="/media/14s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/15.jpg"><img src="/media/15s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/16.jpg"><img src="/media/16s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/17.jpg"><img src="/media/17s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/18.jpg"><img src="/media/18s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/19.jpg"><img src="/media/19s.jpg" alt=""></a></p></div>
-
-	<div class="copy">&copy; 2013 &laquo;<a href="http://www.westvideo.ru">Вест Видео</a>&raquo;. Создание сайта: <a href="http://www.kg-design.ru/">КГ-Дизайн</a></div>
-
-	<div class="cdr">&nbsp;</div>
+	<div class="gallery"><p><a class="stills" data-fancybox-group="gallery_stills" href="/media/01.jpg"><img src="/media/01s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/02.jpg"><img src="/media/02s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/03.jpg"><img src="/media/03s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/04.jpg"><img src="/media/04s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/05.jpg"><img src="/media/05s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/06.jpg"><img src="/media/06s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/07.jpg"><img src="/media/07s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/08.jpg"><img src="/media/08s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/09.jpg"><img src="/media/09s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/10.jpg"><img src="/media/10s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/11.jpg"><img src="/media/11s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/12.jpg"><img src="/media/12s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/13.jpg"><img src="/media/13s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/14.jpg"><img src="/media/14s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/15.jpg"><img src="/media/15s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/16.jpg"><img src="/media/16s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/17.jpg"><img src="/media/17s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/18.jpg"><img src="/media/18s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/19.jpg"><img src="/media/19s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/20.jpg"><img src="/media/20s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/21.jpg"><img src="/media/21s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/22.jpg"><img src="/media/22s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/23.jpg"><img src="/media/23s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/24.jpg"><img src="/media/24s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/25.jpg"><img src="/media/25s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/26.jpg"><img src="/media/26s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/27.jpg"><img src="/media/27s.jpg" alt=""></a><a class="stills" data-fancybox-group="gallery_stills" href="/media/28.jpg"><img src="/media/28s.jpg" alt=""></a></p></div>
 </div>
+
+<div class="copy">&copy; 2013 &laquo;<a href="http://www.westvideo.ru">Вест Видео</a>&raquo;. Создание сайта: <a href="http://www.kg-design.ru/">КГ-Дизайн</a></div>
+
+
 
 <!-- Fancybox -->
 <script type="text/javascript" src="/js/jquery.fancybox.js?v=2.1.1"></script>
